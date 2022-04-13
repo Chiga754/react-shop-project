676ea6dd51e34a7f4ba0737ab9233f50976068b9
Chiga754 <a.chigarev96@gmail.com>
2022-04-13 18:03:56 +0500

    react shop project start

---
+++ src/components/GoodsItem.jsx
@@ -0,0 +1,26 @@
+function GoodsItem(props) {
+    const {
+        id,
+        name,
+        description,
+        price,
+        full_background,
+    } = props;
+
+    return <div className="card" id={id}>
+        <div className="card-image">
+            <img src={full_background} alt={name} />
+
+        </div>
+        <div className="card-content">
+            <span className="card-title">{name}</span>
+            <p>{description}</p>
+        </div>
+        <div className="card-action">
+            <button className="btn" >Купить</button>
+            <span style={{ fontSize: "1.8rem" }} className="right">{price} руб.</span>
+        </div>
+    </div>
+}
+
+export { GoodsItem };
