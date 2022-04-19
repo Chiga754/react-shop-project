711c752ba5c7fbc6127ddcaf0dd733b6c0899153
Aleksandr Chigarev <a.chigarev96@gmail.com>
2022-04-19 07:54:04 +0500

    Added the basket component + refactoring addToBasket

---
+++ src/components/BasketItem.jsx
@@ -0,0 +1,9 @@
+function BasketItem(props) {
+
+    const {id, name, price, quantity} = props;
+
+    return <li className="collection-item">
+        {name} X{quantity} = {price} <span class="secondary-content"><i class="material-icons basket-delete">close</i></span>
+    </li>
+}
+export { BasketItem }
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 function Cart(props) {
-    const {quantity = 0} = props;
+    const {quantity = 0, handleBasketShow = Function.prototype} = props;
 
-    return <div className="cart #607d8b blue-grey">
+    return <div className="cart #607d8b blue-grey" onClick={handleBasketShow}>
         <i className="material-icons">shopping_cart</i>
         {quantity ? <span className="cart-quantity">{quantity}</span> : null}
     </div>
